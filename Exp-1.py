d89a42eaa2c29c9b3cd6dc66b2f051215172b1da
Hassan13102003 <115918799+Hassan13102003@users.noreply.github.com>
2024-02-12 01:43:16 -0800

    Update Exp-1.py

--- Exp-1.py
+++ Exp-1.py
@@ -1,3 +1,11 @@
+#Rabbit Leap Problem
+#Problem Statement:
+# In the rabbit leap problem, three east-bound rabbits stand in a line blocked by three west-bound rabbits. 
+# They are crossing a stream with stones placed in the east west direction in a line.
+#     There is one empty stone between them. Rabbits waiting to cross. Each rabbit can jump over one, but not more than that. 
+#     How can they avoid getting into a deadlock? The rabbits can only move forward one step or two steps. 
+#     They can jump over one rabbit if the need arises, but not more than that. Are they smart enough to cross each other without having to step into the water? 
+#     Draw the state space for solving the problem.
 class Problem:
     def __init__(self):
         pass
